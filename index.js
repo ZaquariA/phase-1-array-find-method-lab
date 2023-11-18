28fab5adb0e1a35071983c9f49c13d96d2185914
zaquari_andl <zaquariandl043@gmail.com>
2023-11-18 15:44:46 -0600

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const record = [
+    {
+        year: "2015",
+        result: "W",
+    },
+    {
+        year: "2014",
+        result: "N/A",
+    },
+    {
+        year: "2013",
+        result: "L",
+    },
+]
+
+function superbowlWin(array) {
+    let win = array.find(function(e) {
+        return e.result === "W"
+    })
+    if (win) {
+        return win.year
+    }
+}
